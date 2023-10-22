ba9aae9d056d501c10a8cbc5c0ebefe98d8bd76a
seulgi <119745417+wisdomSG@users.noreply.github.com>
2023-10-22 21:51:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.26 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int n = s.length();
+
+        // 1. 문자열 회전
+        for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            boolean isValid = true;
+
+            // 2. 스택을 사용하여 올바른 괄호 문자열인지 확인
+            for (int j = 0; j < n; j++) {
+                char c = s.charAt((i + j) % n);
+                if (c == '(' || c == '{' || c == '[') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty() || !isPair(stack.pop(), c)) {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            // 3. 올바른 괄호 문자열이면 answer 증가
+            if (isValid && stack.isEmpty()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    // 4. 괄호의 짝이 맞는지 확인하는 함수
+    private boolean isPair(char open, char close) {
+        return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
+    }
+}
