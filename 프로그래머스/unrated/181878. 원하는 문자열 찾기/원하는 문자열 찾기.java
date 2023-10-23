52ea24d67ce4eb6704f4a80c978ed39c5e2e2ea9
seulgi <119745417+wisdomSG@users.noreply.github.com>
2023-10-23 22:21:13 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 4.46 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(String myString, String pat) {
 
-        String mainStrng = myString.toLowerCase();
+        String mainStrng = myString.toLowerCase(); // 소문자로 변경 : toLowerCase()
         String subString = pat.toLowerCase();
-        if(mainStrng.contains(subString)) {
+        if(mainStrng.contains(subString)) { // 문자열 포함 확인: contains()
             return 1;
         } else {
             return 0;
