7dceed38b4b414d3d11e9b2eacfffdf2d89a039c
seulgi <119745417+wisdomSG@users.noreply.github.com>
2023-11-07 09:21:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 311.83 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        return answer;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for (int i = 0; i < completion.length; i++) {
+            String p = participant[i];
+            String c = completion[i];
+            if (!p.equals(c)) {
+                return participant[i];
+            }
+        }
+
+        return participant[completion.length];
     }
 }
