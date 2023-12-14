595169282539207b047ed813ff14deb2b2a3ef1e
seulgi <119745417+wisdomSG@users.noreply.github.com>
2023-12-14 11:16:41 +0900

    [level 2] Title: 하노이의 탑, Time: 6.09 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private void hanoi(int n, int from, int to, List<int[]> process) {
+        if(n==1) {
+            process.add(new int[] {from, to});
+                return;
+        }
+        
+        int empty = 6 - from - to; // empty:빈기둥, 6=1+2+3
+        hanoi(n-1, from, empty, process);
+        hanoi(1, from, to, process);
+        hanoi(n-1, empty, to, process);
+    }
+    public int[][] solution(int n) {
+        List<int[]> process = new ArrayList<>();
+        hanoi(n, 1, 3, process);
+        return process.toArray(new int[0][]);
+    }
+}
