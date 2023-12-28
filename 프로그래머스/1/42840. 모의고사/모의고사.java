72610a78ba43266e302ae597ed0b10b7fbbb8ad2
seulgi <119745417+wisdomSG@users.noreply.github.com>
2023-12-28 20:44:56 +0900

    [level 1] Title: 모의고사, Time: 4.96 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -8,25 +8,31 @@ class Solution {
         };
     
     public int[] solution(int[] answers) {
-        int[] corrects = new int[3];
-        int max = 0;
-        
-        for(int problem = 0; problem < answers.length; problem++) {
+        // 수포자들의 찍기 규칙 배열
+        // RULES[0]: 1번 수포자, RULES[1]: 2번 수포자, RULES[2]: 3번 수포자
+
+        int[] corrects = new int[3]; // 각 수포자의 정답 개수를 저장하는 배열
+        int max = 0; // 가장 많이 맞힌 정답 개수
+
+        // 문제당 정답 체크
+        for (int problem = 0; problem < answers.length; problem++) {
             int answer = answers[problem];
-            
-            for(int person=0; person<3; person++) {
-                int index = problem % RULES[person].length;
-                int picked = RULES[person][index];
-                if(picked==answer && ++corrects[person]>max) {
-                    max = corrects[person];
+
+            // 각 수포자의 찍기 규칙에 따라 정답을 비교하고 정답 개수 업데이트
+            for (int person = 0; person < 3; person++) {
+                int index = problem % RULES[person].length; // 현재 문제에 대한 찍기 인덱스
+                int picked = RULES[person][index]; // 현재 수포자가 찍은 정답
+                if (picked == answer && ++corrects[person] > max) {
+                    max = corrects[person]; // 가장 많이 맞힌 정답 개수 업데이트
                 }
             }
         }
-        final int maxCorrects = max; 
+        final int maxCorrects = max; // 람다식에서 사용하기 위해 max를 final 변수로 저장
+        
+        // 최대 정답 개수를 가진 수포자들의 번호를 반환
         return IntStream.range(0, 3)
                 .filter(i -> corrects[i] == maxCorrects)
-                .map(i -> i + 1)
+                .map(i -> i + 1) // 인덱스(0-based)를 번호(1-based)로 변환
                 .toArray();
-        
     }
 }
