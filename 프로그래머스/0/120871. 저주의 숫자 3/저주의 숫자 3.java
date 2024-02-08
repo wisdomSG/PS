32429776f0dede60a9b1bd451a8f9fd11043a07e
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-02-08 11:36:50 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.15 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,18 +1,15 @@
 class Solution {
     public int solution(int n) {
-        int[] answer = new int[100];
+        int[] answer = new int[n];
         int num = 1;
-        int i=0;
-        while(i<100) {
-            if(num%3==0 || Integer.toString(num).contains("3")) {
-                num ++;
+        int i = 0;
+        while (i < n) {
+            if (num % 3 == 0 || Integer.toString(num).contains("3")) {
+                num++;
                 continue;
             }
-            answer[i] = num;
-            i++;
-            num ++;
-            
+            answer[i++] = num++;
         }
-        return answer[n-1];
+        return answer[n - 1];
     }
 }
