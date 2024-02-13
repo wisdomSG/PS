68849aed2ec0df52808b393d509a9d2d89398211
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-02-13 11:15:50 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+select board_id, writer_id, title, price,
+    case status
+        when 'SALE' then '판매중'
+        when 'RESERVED' then '예약중'
+        else '거래완료'
+    end as status
+from used_goods_board
+where created_date = '2022-10-05'
+order by board_id desc
