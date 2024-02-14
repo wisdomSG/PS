081ad7e895314db3042d2b63106996a024308136
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-02-14 11:21:04 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.19 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<food.length; i++) {
+            if(food[i]/2>0) {
+                for(int j=0; j<food[i]/2; j++) {
+                    sb.append(i);
+                }
+            }
+        }
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
+}
