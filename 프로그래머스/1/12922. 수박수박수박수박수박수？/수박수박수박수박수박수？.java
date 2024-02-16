212770a6874984e9a8101e2622f0884f5fb510d9
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-02-16 12:21:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            answer.append(i % 2 == 0 ? "박" : "수");
+        }
+        return answer.toString();
+    }
+}
