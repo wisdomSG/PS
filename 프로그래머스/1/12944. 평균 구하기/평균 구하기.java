d86994af12476c6104927051de2ad85919d12ca9
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-02-27 10:47:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,8 +1,10 @@
-import java.util.Arrays;
 
 class Solution {
     public double solution(int[] arr) {
-        double answer = (double) Arrays.stream(arr).sum() / arr.length; 
-        return answer;
+        double answer = 0;
+        for(int num: arr) {
+            answer += num;
+        }
+        return answer/arr.length;
     }
 }
