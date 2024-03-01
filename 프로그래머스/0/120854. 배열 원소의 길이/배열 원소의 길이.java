bac698b950fa4d8629153e8af919f218a30483da
seulgi <119745417+wisdomSG@users.noreply.github.com>
2024-03-01 11:08:13 +0900

    [level 0] Title: 배열 원소의 길이, Time: 1.76 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist)
+                .mapToInt(x -> x.length())
+                .toArray();
+    }
+}
